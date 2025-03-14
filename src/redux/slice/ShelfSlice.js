1a4d114f403e3d0593cf68f49a9a476564d367d2
Kiệt Trần <trantuankiet5980@gmail.com>
2025-03-14 23:34:32 +0700

    load api shelf

---
+++ src/redux/slice/ShelfSlice.js
@@ -0,0 +1,30 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { axiosInstance } from "../../api/APIClient";
+
+export const fetchShelfs = createAsyncThunk(
+  "shelf/fetchShelfs",
+  async ({ page, size, sortBy, sortName }, { rejectWithValue }) => {
+    try {
+      const response = await axiosInstance.get("/api/v1/shelf", {
+        params: { page, size, sortBy, sortName },
+      });
+      return response.data.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const shelfSlice = createSlice({
+  name: "shelf",
+  initialState: { shelves: [], totalPages: 1 },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchShelfs.fulfilled, (state, action) => {
+      state.shelves = action.payload?.data || [];
+      state.totalPages = action.payload?.totalPages || 1;
+    });
+  },
+});
+
+export default shelfSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import UserSlice from './slice/UserSlice';
 import InventoryImportSlice from './slice/InventoryImportSlice';
 import MedicineSlice from './slice/MedicineSlice';
+import ShelfSlice from './slice/ShelfSlice';
 
 const store = configureStore({
     reducer: {
         user: UserSlice,
         inventoryImport: InventoryImportSlice,
         medicine: MedicineSlice,
+        shelf: ShelfSlice,
     },
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware({
